d0d28f3cf80fd6d90722cf1d516e7d556c97d37c
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-10-26 14:15:58 +0530

    activity to fragment done

--- app/src/main/java/com/android/soundlyspotify/MainActivity.kt
+++ app/src/main/java/com/android/soundlyspotify/MainActivity.kt
@@ -10,24 +10,25 @@ import android.widget.Button
 import com.android.soundlyspotify.loginfragment
 
 class MainActivity : AppCompatActivity() {
+    //Button btn;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+       // fragmentTransaction.replace(R.id.container)
 
-        // Replace the fragment_container with the LoginFragment
         val loginFragment = loginfragment()
-        fragmentTransaction.replace(R.id.logincontainer, loginFragment)
+        fragmentTransaction.replace(R.id.container, loginFragment)
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
 
-        // Handle button click to navigate to the LoginFragment
+
         val button = findViewById<Button>(R.id.startbutton)
         button.setOnClickListener {
             val loginFragment = loginfragment()
-            fragmentTransaction.replace(R.id.logincontainer, loginFragment)
+            fragmentTransaction.replace(R.id.container, loginFragment)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
