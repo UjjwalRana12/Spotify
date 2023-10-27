c6bf6d54756ffceba82aaede21d8dd1f764f8cd9
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-10-27 22:51:34 +0530

    layout work completed

--- app/src/main/java/com/android/soundlyspotify/signupfragment.kt
+++ app/src/main/java/com/android/soundlyspotify/signupfragment.kt
@@ -19,7 +19,7 @@ class signupfragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    @SuppressLint("SuspiciousIndentation")
+    @SuppressLint("SuspiciousIndentation", "MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,25 +27,25 @@ class signupfragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_signupfragment, container, false)
 
-        val button = view.findViewById<Button>(R.id.contbutton)
+        val button = view.findViewById<Button>(R.id.cont1button)
         button.setOnClickListener {
             val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupphone,otpfragment())
+            fragmentTransaction.replace(R.id.signupfrag,otpfragment())
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
 
-        val textbutton2 = view.findViewById<TextView>(R.id.emailtext)
+        val textbutton2 = view.findViewById<TextView>(R.id.emailtext1)
         textbutton2.setOnClickListener {
             val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupphone, signupphonefragment())
+            fragmentTransaction.replace(R.id.signupfrag, signupphonefragment())
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
-        val textbutton = view.findViewById<TextView>(R.id.loginbutton)
+        val textbutton = view.findViewById<TextView>(R.id.loginbutton1)
           textbutton.setOnClickListener {
             val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupphone,loginfragment())
+            fragmentTransaction.replace(R.id.signupfrag,loginfragment())
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
