7621687b2a3aaf217e9fb297292225a0c987c1fc
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-10-29 04:28:51 +0530

    phone number response sent

--- app/src/main/java/com/android/soundlyspotify/signupfragment.kt
+++ app/src/main/java/com/android/soundlyspotify/signupfragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import signupphonefragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
--- app/src/main/java/com/android/soundlyspotify/signupphonefragment.kt
+++ app/src/main/java/com/android/soundlyspotify/signupphonefragment.kt
@@ -1,53 +1,96 @@
-package com.android.soundlyspotify
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.android.soundlyspotify.ApiResponse
+import com.android.soundlyspotify.PhoneRegistrationRequest
+import com.android.soundlyspotify.R
+import com.android.soundlyspotify.RetrofitClient
+import com.android.soundlyspotify.otpfragment
+import com.android.soundlyspotify.signupfragment
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-class signupphonefragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class SignupPhoneFragment : Fragment() {
+    private val userAPI = RetrofitClient.userAPI
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_signupphonefragment, container, false)
 
         val button = view.findViewById<Button>(R.id.cont2button)
+        val usernameEditText = view.findViewById<EditText>(R.id.supusername)
+        val phoneNumberEditText = view.findViewById<EditText>(R.id.supphone)
+
         button.setOnClickListener {
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupphonefrag, otpfragment())
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
+            val username = usernameEditText.text.toString().trim()
+            val phoneNumber = phoneNumberEditText.text.toString().trim()
+
+            if (validateData(username, phoneNumber)) {
+                val phoneRegistrationRequest = PhoneRegistrationRequest(username, phoneNumber)
+
+                userAPI.registerByPhone(phoneRegistrationRequest).enqueue(object : Callback<ApiResponse> {
+                    override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                        if (response.isSuccessful) {
+                            val responseBody = response.body()
+                            if (responseBody != null && responseBody.success) {
+                                // Registration successful, navigate to OTP page
+                                val fragmentTransaction = parentFragmentManager.beginTransaction()
+                                fragmentTransaction.replace(R.id.signupphonefrag, otpfragment())
+                                fragmentTransaction.addToBackStack(null)
+                                fragmentTransaction.commit()
+                            } else {
+                                showToast("Registration failed. Please try again.")
+                            }
+                        } else {
+                            showToast("Registration failed. Please try again.")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                        // Handle network errors or other failures
+                        showToast("Network error. Please check your internet connection.")
+                    }
+                })
+            }
         }
+
         val textbutton2 = view.findViewById<TextView>(R.id.emailtext2)
         textbutton2.setOnClickListener {
             val fragmentTransaction = parentFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.signupphonefrag, signupfragment())
-
+            fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
+
         return view
+    }
+
+    private fun validateData(username: String, phoneNumber: String): Boolean {
+        if (username.isEmpty()) {
+            showToast("Username cannot be empty")
+            return false
+        }
+
+        if (phoneNumber.isEmpty()) {
+            showToast("Phone number cannot be empty")
+            return false
+        }
+
+        // Add more specific validation logic if needed
+
+        return true
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
