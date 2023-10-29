3879fa5d10a95444c739109b63cf5d6ce32e4289
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-10-29 21:09:36 +0530

    signup fragmnet updated

--- app/src/main/java/com/android/soundlyspotify/RetrofitClient.kt
+++ app/src/main/java/com/android/soundlyspotify/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://127.0.0.1:8000"
+    private const val BASE_URL = "https://test-mkcw.onrender.com"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
--- app/src/main/java/com/android/soundlyspotify/signupfragment.kt
+++ app/src/main/java/com/android/soundlyspotify/signupfragment.kt
@@ -1,53 +1,78 @@
 package com.android.soundlyspotify
+
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import signupphonefragment
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class signupfragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     @SuppressLint("SuspiciousIndentation", "MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_signupfragment, container, false)
 
-        val button = view.findViewById<Button>(R.id.cont1button)
-        button.setOnClickListener {
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupfrag,otpfragment())
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
+        val usernameEditText = view.findViewById<EditText>(R.id.editTextText3)
+        val emailEditText = view.findViewById<EditText>(R.id.editTextText4)
+
+        val signupButton = view.findViewById<Button>(R.id.signupButton)
+        signupButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val email = emailEditText.text.toString()
+
+            val userAPI = RetrofitClient.userAPI
+            val userRegistrationRequest = EmailRegistrationRequest(username, email)
+
+            try {
+                val call = userAPI.registerByEmail(userRegistrationRequest)
+                call.enqueue(object : Callback<ApiResponse> {
+                    override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                        if (response.isSuccessful) {
+                            Log.d("api", "Success")
+                            val fragmentTransaction = parentFragmentManager.beginTransaction()
+                            fragmentTransaction.replace(R.id.signupfrag, otpfragment())
+                            fragmentTransaction.addToBackStack(null)
+                            fragmentTransaction.commit()
+                        } else {
+                            Log.d("api", "Unsuccessful")
+                            Toast.makeText(requireContext(), "Something went wrong.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                        Log.e("api", "API call failed: ${t.message}") // Log the reason for failure
+                        Toast.makeText(requireContext(), "API call failed. Please try again.", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            } catch (e: Exception) {
+                Log.e("api", "Exception: ${e.message}") // Log exceptions
+                Toast.makeText(requireContext(), "Exception: " + e.message, Toast.LENGTH_SHORT).show()
+            }
         }
 
         val textbutton2 = view.findViewById<TextView>(R.id.emailtext1)
         textbutton2.setOnClickListener {
             val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupfrag,signupphonefragment())
-
+            fragmentTransaction.replace(R.id.signupfrag, signupphonefragment())
             fragmentTransaction.commit()
         }
-        val textbutton = view.findViewById<TextView>(R.id.loginbutton1)
-          textbutton.setOnClickListener {
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.signupfrag,loginfragment())
 
+        val textbutton = view.findViewById<TextView>(R.id.loginbutton1)
+        textbutton.setOnClickListener {
+            val fragmentTransaction = parentFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.signupfrag, loginfragment())
             fragmentTransaction.commit()
         }
 
