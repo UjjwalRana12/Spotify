061282158310a4ceb55aad9f8b399c4b0dc2067c
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-10-31 21:49:03 +0530

    username api integrated

--- app/src/main/java/com/android/soundlyspotify/usernamelogin.kt
+++ app/src/main/java/com/android/soundlyspotify/usernamelogin.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -33,6 +36,44 @@ class usernamelogin : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_usernamelogin, container, false)
 
+        val usernameEditText = view.findViewById<EditText>(R.id.userTextText4)
+        val button = view.findViewById<Button>(R.id.contbutton)
+
+        button.setOnClickListener {
+            val username = usernameEditText.text.toString()
+
+            if (username.isEmpty()) {
+                Toast.makeText(requireContext(), "Username cannot be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val userLoginRequest = UserLoginRequest(username)
+            val userAPI = RetrofitClient.userAPI
+            val call = userAPI.userLogin(userLoginRequest)
+
+            call.enqueue(object : Callback<ApiResponse> {
+                override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                    if (response.isSuccessful) {
+                        val apiResponse = response.body()
+                        if (apiResponse != null && apiResponse.success) {
+                            val fragmentTransaction = parentFragmentManager.beginTransaction()
+                            fragmentTransaction.replace(R.id.usernamelogin, otpfragment())
+                            fragmentTransaction.addToBackStack(null)
+                            fragmentTransaction.commit()
+                        } else {
+                            Toast.makeText(requireContext(), "Login failed", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(requireContext(), "Login failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                    Toast.makeText(requireContext(), "API call failed. Please try again.", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
         val textButton = view.findViewById<TextView>(R.id.forgotView2)
         textButton.setOnClickListener {
             val fragmentTransaction = parentFragmentManager.beginTransaction()
@@ -40,14 +81,6 @@ class usernamelogin : Fragment() {
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
-        val button = view.findViewById<Button>(R.id.contbutton)
-        button.setOnClickListener {
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.usernamelogin,otpfragment())
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
-        }
-
 
         return view
     }
