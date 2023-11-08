676124d4823717a09ecb0e7b8f756dd7333cc489
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-11-09 01:43:10 +0530

    view pager added

--- app/src/main/java/com/android/soundlyspotify/homefragment.kt
+++ app/src/main/java/com/android/soundlyspotify/homefragment.kt
@@ -2,20 +2,25 @@ package com.android.soundlyspotify
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
 import com.android.soundlyspotify.Myadapter.BestSeller2Adapter
 import com.android.soundlyspotify.Myadapter.BestSellerAdapter
 import com.android.soundlyspotify.Myadapter.ClothingAdapter
+import com.android.soundlyspotify.Myadapter.ImagePagerAdapter
 import com.android.soundlyspotify.Myadapter.OfferAdapter
 import com.android.soundlyspotify.models.BestSeller
 import com.android.soundlyspotify.models.BestSeller2
 import com.android.soundlyspotify.models.Clothing
 import com.android.soundlyspotify.models.Offer
+import java.util.Timer
+import java.util.TimerTask
 
 class homefragment : Fragment() {
 
@@ -23,6 +28,15 @@ class homefragment : Fragment() {
     private lateinit var bestsellerRecyclerView: RecyclerView
     private lateinit var clothingRecyclerView: RecyclerView
     private lateinit var bestseller2RecyclerView: RecyclerView
+    private lateinit var viewPager: ViewPager
+    private lateinit var imageAdapter: ImagePagerAdapter
+    private val imageList = listOf(
+        R.drawable.mobileslider,
+        R.drawable.mobileslider,
+        R.drawable.mobileslider
+    )
+    private var currentPage = 0
+    private val timer = Timer()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +45,29 @@ class homefragment : Fragment() {
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_homefragment, container, false)
+
+        //View pager
+        viewPager = view.findViewById(R.id.viewPager)
+        imageAdapter = ImagePagerAdapter(requireContext(), imageList)
+        viewPager.adapter = imageAdapter
+
+        val handler = Handler()
+        val update = Runnable {
+            if (currentPage == imageList.size) {
+                currentPage = 0
+            }
+            viewPager.currentItem = currentPage++
+        }
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                handler.post(update)
+            }
+        }, 2000, 3000)
+
+
+
+
 
         // Initialize RecyclerViews and set their properties
         offerRecyclerView = view.findViewById(R.id.offerRecyclerView)
