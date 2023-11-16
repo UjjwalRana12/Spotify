ee9dd58508ca3b333df86cd2609f48d0495199db
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-11-16 15:37:08 +0530

    search frag completed

--- app/src/main/java/com/android/soundlyspotify/search.kt
+++ app/src/main/java/com/android/soundlyspotify/search.kt
@@ -1,7 +1,8 @@
 package com.android.soundlyspotify
-
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import android.view.LayoutInflater
@@ -13,9 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.appcompat.widget.SearchView
 import com.android.soundlyspotify.applied_api.RetroClient
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class search : Fragment() {
 
@@ -51,6 +49,7 @@ class search : Fragment() {
                     Log.d(TAG, "Delay completed")
                     newText?.let {
                         Log.d(TAG, "Calling searchSongs with query: $it")
+                        hideErrorFragment()  // Hide the error fragment when a new search is initiated
                         searchSongs(it)
                     }
                 }
@@ -63,7 +62,6 @@ class search : Fragment() {
         return view
     }
 
-
     private fun searchSongs(query: String) {
         lifecycleScope.launch {
             try {
@@ -72,22 +70,72 @@ class search : Fragment() {
                 if (apiResponse.success) {
                     Log.d(TAG, "API call successfully")
                     val apiResults = apiResponse.data
-                    songAdapter.setData(apiResults.toMutableList()) // Convert to MutableList
-                    Log.d(TAG, "Search successful. Number of results: ${apiResults.size}")
+                    if (apiResults.isNotEmpty()) {
+                        songAdapter.setData(apiResults.toMutableList())
+                        Log.d(TAG, "Search successful. Number of results: ${apiResults.size}")
+                    } else {
+                        showEmptyResultFragment()
+                    }
                 } else {
-                    // Log the failure message from the API response
+                    showErrorFragment()
                     Log.e(TAG, "Search failed. API message: ${apiResponse.message}")
                 }
             } catch (e: HttpException) {
-                // Handle HTTP errors
                 when (e.code()) {
-                    404 -> Log.e(TAG, "Resource not found: ${e.message()}")
-                    else -> Log.e(TAG, "HTTP error: ${e.code()} ${e.message()}")
+                    404 -> {
+                        showNotFoundErrorFragment()
+                        Log.e(TAG, "Resource not found: ${e.message()}")
+                    }
+                    else -> {
+                        showErrorFragment()
+                        Log.e(TAG, "HTTP error: ${e.code()} ${e.message()}")
+                    }
                 }
             } catch (e: Exception) {
-                // Handle general exceptions
+                showErrorFragment()
                 Log.e(TAG, "Error fetching data: ${e.message}")
             }
+        }
+    }
+
+    private fun showEmptyResultFragment() {
+        val transaction = parentFragmentManager.beginTransaction()
+
+        // Replace the current fragment with the not found fragment and add a tag
+        val notFoundFragment = aayein()
+        transaction.addToBackStack("not_found")
+        transaction.replace(R.id.aayein_frame, notFoundFragment, "not_found")
+        transaction.commit()
+    }
+
+    private fun showNotFoundErrorFragment() {
+        val transaction = parentFragmentManager.beginTransaction()
+
+        // Replace the current fragment with the not found fragment and add a tag
+        val notFoundFragment = aayein()
+        transaction.addToBackStack("not_found")
+        transaction.replace(R.id.aayein_frame, notFoundFragment, "not_found")
+        transaction.commit()
+    }
+    private fun showErrorFragment() {
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.addToBackStack("error_fragment")
+        // Replace the current fragment with the error fragment and add a tag
+        val errorFragment = aayein()
+
+        transaction.replace(R.id.aayein_frame, errorFragment, "error_fragment")
+        transaction.commit()
+    }
+
+    private fun hideErrorFragment() {
+        val fragmentManager = parentFragmentManager
+        val errorFragment = fragmentManager.findFragmentByTag("not_found")
+
+        errorFragment?.let {
+            val transaction = fragmentManager.beginTransaction()
+
+            transaction.remove(it)
+            transaction.commit()
         }
     }
 
